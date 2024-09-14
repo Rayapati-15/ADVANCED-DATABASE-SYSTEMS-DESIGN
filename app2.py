ba17829672b1309d9a38ab7a22a764f5b62eb9a4
Rayapati-15 <144276156+Rayapati-15@users.noreply.github.com>
2024-09-14 13:41:17 -0400

    app2.py

--- app2.py
+++ app2.py
@@ -1,15 +1,15 @@
 import sqlite3
 # Connect to a database (or create one if it doesn't exist)
-connection = sqlite3.connect('example2.db')
+connection = sqlite3.connect('Example2.db')
 # Create a cursor object to interact with the database
 cursor = connection.cursor()
-cursor.execute('''CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, grade REAL)''')
-cursor.execute("INSERT INTO students (id,name, grade) VALUES (118,'RUBY', 85.5)")
-cursor.execute("INSERT INTO students (id,name, grade) VALUES (120,'Rambo', 90.0)")
-cursor.execute("INSERT INTO students (id,name, grade) VALUES (122,'Rio', 87.5)")
-cursor.execute("INSERT INTO students (id,name, grade) VALUES (124,'Sam', 70.5)")
-cursor.execute("INSERT INTO students (id,name, grade) VALUES (126,'Jaggu', 90.5)")
-cursor.execute("SELECT * FROM students")
+cursor.execute('''CREATE TABLE Students (Id INTEGER PRIMARY KEY, name TEXT, grade REAL)''')
+cursor.execute("INSERT INTO Students (Id,Name, Grade) VALUES (118,'RUBY', 85.5)")
+cursor.execute("INSERT INTO Students (Id,Name, Grade) VALUES (120,'Rambo', 90.0)")
+cursor.execute("INSERT INTO Students (Id,Name, Grade) VALUES (122,'Rio', 87.5)")
+cursor.execute("INSERT INTO Students (Id,Name, Grade) VALUES (124,'Sam', 70.5)")
+cursor.execute("INSERT INTO Students (Id,Name, Grade) VALUES (126,'Jaggu', 90.5)")
+cursor.execute("SELECT * FROM Students")
 rows = cursor.fetchall()
 for row in rows:
   print(row)
