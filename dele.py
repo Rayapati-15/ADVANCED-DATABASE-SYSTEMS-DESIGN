f178b68d6131475b14262248109e04831ba1cd12
Rayapati-15 <144276156+Rayapati-15@users.noreply.github.com>
2024-09-14 13:58:48 -0400

    Update dele.py

--- dele.py
+++ dele.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-conn = sqlite3.connect('student.db')
+conn = sqlite3.connect('Student.db')
 cursor = conn.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS students")
+cursor.execute("DROP TABLE IF EXISTS Students")
 
 conn.commit()
 conn.close()
